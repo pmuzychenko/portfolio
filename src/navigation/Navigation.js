cee121a6696a209439318731ccde3207e3a3610d
pmuzychenko <pmuzychenko@gmail.com>
2021-04-13 21:11:39 +0300

    feat: add changes to styles into Button and Navigation component

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import {Link} from "react-scroll";
 import styles from './Navigation.module.css';
 
 function Navigation() {
+    const items = ['home', 'skills', 'projects', 'contacts']
     return (
-        <div className={styles.nav}>
-            <a href>Home</a>
-            <a href>Skills</a>
-            <a href>Projects</a>
-            <a href>Contacts</a>
+        <div className={styles.navigation}>
+            <div className={styles.container}>
+                {items.map((item, index) =>
+                    <Link
+                        key={index}
+                        className={styles.link}
+                        activeClass={styles.active}
+                        to={item}
+                        spy={true}
+                        smooth={true}
+                        offset={-99}
+                        duration={500}
+                    >{item}</Link>
+                )}
+            </div>
         </div>
     );
 }
