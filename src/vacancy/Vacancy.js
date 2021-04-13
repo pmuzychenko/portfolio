4876c32a19600d07f85760b95275f88bcd87156e
pmuzychenko <pmuzychenko@gmail.com>
2021-04-13 14:30:31 +0300

    feat: add styles changes to Button component

--- src/vacancy/Vacancy.js
+++ src/vacancy/Vacancy.js
@@ -2,13 +2,14 @@ import React from 'react'
 import stylesContainer from '../common/styles/Container.module.css'
 import styles from './Vacancy.module.css'
 import Title from "../common/title/Title";
+import Button from "../common/button/Button";
 
 function Vacancy() {
     return (
         <div className={styles.vacancyBlock}>
             <div className={`${stylesContainer.container} ${styles.vacancyContainer}`}>
                 <Title title={'I Am Available For Freelance'}/>
-                <button>HIRE ME</button>
+                <Button title={'hire me'}/>
             </div>
         </div>
     );
