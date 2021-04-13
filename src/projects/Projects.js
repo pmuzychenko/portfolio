97493ccd090f43a7c7c9c27c8c81666135383268
pmuzychenko <pmuzychenko@gmail.com>
2021-04-13 14:53:38 +0300

    feat: add styles changes to Projects and Project components

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -13,12 +13,55 @@ function Projects() {
                 </div>
 
                 <div className={styles.projects}>
-                    <Project title={"Project 1"} description={"Project description will be here "}
-                             link={"link #1"} titleLink={"here will be img-link"}/>
-                    <Project title={"Project 2"} description={"Project description will be here"}
-                             link={"link #2"} titleLink={"here will be img-link"}/>
-                    <Project title={"Project 3"} description={"Project description will be here"}
-                             link={"link #3"} titleLink={"here will be img-link"}/>
+                    <div className={styles.mainSecond}>
+                        <div className={styles.secondSmall}>
+                            <Project title={"The Social Network"} description={"Project description will be here "}
+                                     link={"link #1"} titleLink={"here will be img-link"}/>
+                        </div>
+                        <div className={styles.secondBig}>
+                            <Project title={"Todolist"} description={"Project description will be here"}
+                                     link={"link #2"} titleLink={"here will be img-link"}/>
+                        </div>
+                    </div>
+
+                    <div className={styles.mainSecond}>
+                        <div className={styles.secondBig}>
+                            <Project title={"The Social Network"}
+                                     description={"Project description will be here"}
+                                     link={"link #4"} titleLink={"here will be img-link"}/>
+                        </div>
+                        <div className={styles.secondSmall}>
+                            <Project title={"Counter"} description={"Project description will be here"}
+                                     link={"link #3"} titleLink={"here will be img-link"}/>
+                        </div>
+                    </div>
+
+
+                    <div className={styles.mainSecond}>
+                        <div className={styles.secondSmall}>
+                            <Project title={"Friday Project"}
+                                     description={"Project description will be here"}
+                                     link={"link #5"} titleLink={"here will be img-link"}/>
+                        </div>
+                        <div className={styles.secondBig}>
+                            <Project title={"Friday Project"}
+                                     description={"Project description will be here"}
+                                     link={"link #6"} titleLink={"here will be img-link"}/>
+                        </div>
+                    </div>
+
+                    <div className={styles.mainSecond}>
+                        <div className={styles.secondBig}>
+                            <Project title={"The Social Network"}
+                                     description={"Project description will be here"}
+                                     link={"link #1"} titleLink={"here will be img-link"}/>
+                        </div>
+                        <div className={styles.secondSmall}>
+                            <Project title={"Todolist"} description={"Project description will be here"}
+                                     link={"link #2"} titleLink={"here will be img-link"}/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
